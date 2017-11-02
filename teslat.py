a11a9d22ba96d679840936da450b692e17d602c1
Lingyun Yang <lingyun.yang@gmail.com>
2017-11-01 21:47:54 -0400

    tested with some .lte files

--- teslat.py
+++ teslat.py
@@ -2,6 +2,10 @@ import sys
 import re
 import ply.lex as lex
 
+
+"""
+- lte may parse 'C6: KICKER,,L=0.15', but not here
+"""
 debug = 0
 keywords = ('LINE', 'INCLUDE', 'SAVE')
 tokens = keywords + (
@@ -37,6 +41,11 @@ def t_NEWLINE(t):
     r'&?\n'
     t.lexer.lineno += 1
     #return t
+
+def t_COMMENT(t):
+    r'!.*\n'
+    t.lexer.lineno += 1
+    pass
 
 def t_error(t):
     print("Illegal character %s" % t.value[0])
@@ -84,6 +93,12 @@ elements = {}
 _the_line_ = None
 lines = {}
 
+def ucase_set(d, k, v):
+    d[k.upper()] = v
+
+def ucase_get(d, k, val=None):
+    return d.get(k.upper(), val)
+
 def p_statement_list(p):
     '''statements : 
                   | statements assignment
@@ -94,21 +109,23 @@ def p_statement_list(p):
         tag = p[2][0]
         if tag == 'ASSIGN' or tag == 'VASSIGN':
             name, val = p[2][1:]
-            variables[name] = val
+            ucase_set(variables, name, val)
         elif tag == 'ELEMENT':
             #print(p[1], type(p[2]), p[2], p[2][1])
             name, elemtype, prpts = p[2][1:]
-            elements[name] = p[2]
+            ucase_set(elements, name, p[2])
         elif tag == 'LINE':
             # print('LINE', p[2])
             name, elemlist = p[2][1:]
-            lines[name] = elemlist
+            ucase_set(lines, name, elemlist)
             _the_line_ = name
+        print(p[2])
         # print_nested_tuple(p[2], 0)
     pass
 
 def p_assignment_assign(p):
-    '''assignment : ID EQUALS expression'''
+    '''assignment : ID EQUALS expression
+                  | ID EQUALS STRING'''
     p[0] = ('ASSIGN', p[1], p[3])
 
 
@@ -179,6 +196,11 @@ def p_element(p):
     '''element : ID ":" ID'''
     p[0] = ('ELEMENT', p[1], p[3], {})
 
+def p_element_elegant(p):
+    '''element : STRING ":" ID'''
+    # elegant allows '"LIN_P" : Drift'
+    p[0] = ('ELEMENT', p[1][1:-1], p[3], {})
+    
 def p_element_properties(p):
     '''element : element COMMA assignment'''
     tag, name, elemtype, prpts = p[1]
@@ -193,11 +215,14 @@ def p_elemlist_bin(p):
                 | INTEGER TIMES elemlist'''
 
     if len(p) == 2:
-        if p[1] in lines:
-            p[0] = tuple([p[1] + '.' + x for x in lines[p[1]]])
-        elif p[1] in elements:
+        if ucase_get(lines, p[1], None):
+            elemline = ucase_get(lines, p[1])
+            p[0] = tuple([p[1] + '.' + x for x in elemline])
+        elif ucase_get(elements, p[1], None):
             p[0] = (p[1],)
         else:
+            print(lines.keys())
+            print(elements.keys())
             raise RuntimeError("can not find element/line '%s'" % p[1])
     elif len(p) == 3:
         p[0] = tuple(['-' + x for x in reversed(p[2]) ])
@@ -216,6 +241,9 @@ def p_beamline(p):
     '''beamline : ID ":" LINE EQUALS LPAREN elemlist RPAREN'''
     p[0] = ('LINE', p[1], p[6])
 
+def p_beamline_ltepatch(p):
+    '''beamline : STRING ":" LINE EQUALS LPAREN elemlist RPAREN'''
+    p[0] = ('LINE', p[1][1:-1], p[6])
     
 def p_error(p):
     if p:
@@ -230,9 +258,9 @@ def expand_line(line, elemdict):
         name = elem.replace('-', '')
         direction = (len(elem) - len(name)) % 2
         fam = name.split('.')
-        if fam[-1] not in elemdict:
+        if not ucase_get(elemdict, fam[-1]):
             raise RuntimeError("can not find element '%s'" % fam[-1])
-        tag, origname, elemtype, prpts = elemdict[fam[-1]]
+        tag, origname, elemtype, prpts = ucase_get(elemdict, fam[-1])
         fuprpts = prpts.copy()
         fuprpts.update({'.TYPE': elemtype, '.NAME': fam[-1], '.FULL_NAME': elem, '.DIRECTION': direction})
         fuline.append(fuprpts)
@@ -266,7 +294,7 @@ print('Elements:\n', json.dumps(elements, sort_keys=True, indent=4, separators=(
 #print('Lines:\n', json.dumps(lines, sort_keys=True, indent=4, separators=(',', ':')))
 print("the line:", _the_line_)
 if _the_line_:
-    fuline = expand_line(lines[_the_line_], elements)
+    fuline = expand_line(ucase_get(lines, _the_line_), elements)
     print('Line:\n', json.dumps(fuline, sort_keys=True, indent=4, separators=(',', ':')))
 
 
